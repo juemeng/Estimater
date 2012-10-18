2a278423dad06db3a3c8c4d7806c2a51e126a056
Jo <juemeng@gmail.com>
2012-10-18 17:16:21 +0800

    Finish Logic

---
+++ Estimater/ServiceHelper.h
@@ -0,0 +1,13 @@
+//
+//  ServiceHelper.h
+//  Estimater
+//
+//  Created by Jo on 12-10-18.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServiceHelper : NSObject
+
+@end
---
+++ Estimater/ServiceHelper.m
@@ -0,0 +1,13 @@
+//
+//  ServiceHelper.m
+//  Estimater
+//
+//  Created by Jo on 12-10-18.
+//
+//
+
+#import "ServiceHelper.h"
+
+@implementation ServiceHelper
+
+@end
